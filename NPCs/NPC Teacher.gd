9c27b8439106fadd76c0fca701524984da89234b
Leonardo Sánchez Del Toro <leonardo.sanchez@upnay.edu.mx>
2022-11-25 13:56:45 -0700

    Changes in the NPCs dialogs
    NPC Teacher added.
    Label for dialogs fixed.
    Added dialog for teacher.

--- NPCs/NPC Teacher.gd
+++ NPCs/NPC Teacher.gd
@@ -3,8 +3,8 @@ extends Area2D
 var active = false
 
 func _ready():
-	connect("body_entered", self, '_on_NPC_Student_body_entered')
-	connect("body_exited", self, '_on_NPC_Student_body_exited')
+	connect("body_entered", self, '_on_NPC_Teacher_body_entered')
+	connect("body_exited", self, '_on_NPC_Teacher_body_exited')
 
 func _process(delta):
 	$KeySprite.visible = active
@@ -13,7 +13,7 @@ func _input(event):
 	if get_node_or_null('DialogNode') == null:
 		if event.is_action_pressed("interact") and active:
 			get_tree().paused = true
-			var dialog = Dialogic.start('student1-timeline')
+			var dialog = Dialogic.start('teacher1-timeline')
 			dialog.pause_mode = Node.PAUSE_MODE_PROCESS
 			dialog.connect('timeline_end',self, 'unpause')
 			add_child(dialog)
