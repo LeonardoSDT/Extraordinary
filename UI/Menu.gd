1ac49b7bf9af276f616f30dd2fc2c80d3d2bdb23
Leonardo Sánchez Del Toro <leonardo.sanchez@upnay.edu.mx>
2022-12-01 12:32:04 -0700

    Added more sounds and soundtracks
    - Added pause, unpause, menu focus and menu select sounds.
    - Added inside library soundtrack.

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -1,13 +1,33 @@
 extends Control
 
+var menu = preload("res://Music and Sounds/Menu Move.wav")
+var select = preload("res://Music and Sounds/Menu Select.wav")
+
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_StartButton_pressed():
+	$AudioStreamPlayer.stream = select
+	$AudioStreamPlayer.play()
 	get_tree().change_scene("res://World.tscn")
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
 
 func _on_OptionsButton_pressed():
+	$AudioStreamPlayer.stream = select
+	$AudioStreamPlayer.play()
 	get_tree().change_scene("res://UI/OptionsMenu.tscn")
+
+
+func _on_StartButton_focus_entered():
+	$AudioStreamPlayer.stream = menu
+	$AudioStreamPlayer.play()
+
+func _on_OptionsButton_focus_entered():
+	$AudioStreamPlayer.stream = menu
+	$AudioStreamPlayer.play()
+
+func _on_QuitButton_focus_entered():
+	$AudioStreamPlayer.stream = menu
+	$AudioStreamPlayer.play()
--- UI/Pause.gd
+++ UI/Pause.gd
@@ -1,12 +1,26 @@
 extends Control
 
+var pause = preload("res://Music and Sounds/Pause.wav")
+var unpause = preload("res://Music and Sounds/Unpause.wav")
+
+func _ready():
+	$BlackOverlay/VBoxContainer/Resume.grab_focus()
+
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
+		if new_pause_state == true:
+			$AudioStreamPlayer.stream = pause
+			$AudioStreamPlayer.play()
+		else:
+			$AudioStreamPlayer.stream = unpause
+			$AudioStreamPlayer.play()
 
 func _on_Resume_pressed():
+	$AudioStreamPlayer.stream = unpause
+	$AudioStreamPlayer.play()
 	var new_pause_state = not get_tree().paused
 	get_tree().paused = new_pause_state
 	visible = new_pause_state
